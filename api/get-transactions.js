1f1ec923d3fea859fb2e3ce2fe92ee53be541b71
danig9191 <goldsteindani1@gmail.com>
2026-05-12 13:58:53 +0300

    Create get-transactions.js

---
+++ api/get-transactions.js
@@ -0,0 +1,24 @@
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
+const client = new PlaidApi(new Configuration({
+  basePath: PlaidEnvironments[process.env.PLAID_ENV],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
+    },
+  },
+}));
+
+export default async function handler(req, res) {
+  const { access_token } = req.body;
+  const now = new Date();
+  const start = new Date();
+  start.setFullYear(now.getFullYear() - 1);
+  const response = await client.transactionsGet({
+    access_token,
+    start_date: start.toISOString().split('T')[0],
+    end_date: now.toISOString().split('T')[0],
+  });
+  res.json(response.data);
+}
